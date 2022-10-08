2cb0ee89a3f666ab110f360a860ce14c22e2f485
GeRgOProg <kereszturigergo1122@gmail.com>
2022-10-08 22:21:37 +0200

    Branch initial setup commit

---
+++ Projekt/WorkerDashboard/app/build/generated/ap_generated_sources/debug/out/hu/pannon/workerdashboard/BR.java
@@ -0,0 +1,5 @@
+package hu.pannon.workerdashboard;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ Projekt/WorkerDashboard/app/build/generated/data_binding_trigger/debug/hu/pannon/workerdashboard/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package hu.pannon.workerdashboard;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
